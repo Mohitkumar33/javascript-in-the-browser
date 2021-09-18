57b3cd98d531aee5034afb6babf56fcb1fc2bdd2
Mohit kumar <46507817+Mohitkumar33@users.noreply.github.com>
2021-09-18 18:57:40 +0530

    till testing you are in box

--- app.js
+++ app.js
@@ -4,13 +4,38 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?";
+
+function getTranslationURL(text)
+{
+    return serverURL+"text="+text;
+}
+
+function errorHandler(error)
+{
+    console.log("error occured", error);
+    alert("something wrong with server! try after some time");
+}
+
+
 
 
 function clickHandler()
 {
-    console.log("clicked");
-    console.log("input", txtInput.value);
-    outputDiv.innerText = "yo yo " +txtInput.value;
+    // console.log("clicked");
+    // console.log("input", txtInput.value);
+    // outputDiv.innerText = "yo yo " +txtInput.value;
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response =>response.json())
+        // .then(json => console.log(json.contents.translated))
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click",clickHandler);
